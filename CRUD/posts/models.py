6ebb28230c11e822fed7638822e48c2acdac194a
yb5464 <kyb3634@naver.com>
2021-01-14 12:33:06 +0900

    post_list READ

--- CRUD/posts/models.py
+++ CRUD/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name="제목", max_length=100)
+    writer = models.CharField(verbose_name="글쓴이", max_length=50)
+    content = models.TextField(verbose_name="내용")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title    
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return HttpResponse("글 리스트")
+    '''
+    Read(R)
+    포스트들을 불러와서 리스트형태로 보여준다.
+    '''
+
+    posts = Post.objects.all()
+    ctx = {'posts': posts}
+
+    return render(request, template_name="posts/list.html", context=ctx)
