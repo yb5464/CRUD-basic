34ea17374fef137ef1536d3d34d6f66263f29159
yb5464 <kyb3634@naver.com>
2021-01-14 15:00:06 +0900

    create_post CREATE

--- CRUD/posts/urls.py
+++ CRUD/posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+#name space를 의미함
+app_name = 'posts'
+
 urlpatterns = [
     path('', view=views.post_list, name='list'),
     path('<int:post_id>/', view = views.post_detail, name="detail"),
+    path('create/', view = views.create_post, name="create"),
 ]
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -24,3 +25,23 @@ def post_detail(request, post_id):
     ctx = {'post': post}
 
     return render(request, template_name="posts/detail.html", context=ctx)
+
+def create_post(request):
+    '''
+    Create(C)
+    포스트를 새로 생성한다
+    request method ==> GET, POST
+    '''
+
+    if request.method == 'POST':
+        #글쓰기 칸을 저장할 때 '저장하기' 버튼을 눌렀을 때
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:list')
+    else:
+        #글쓰기 칸을 보여줄 때
+        form = PostForm()
+        ctx = {'form': form}
+
+        return render(request, template_name='posts/post_form.html', context=ctx)
