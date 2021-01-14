2bde417dae8c7c84f5085cff193a59c8037d33de
yb5464 <kyb3634@naver.com>
2021-01-14 14:03:22 +0900

    post_detail READ

--- CRUD/posts/urls.py
+++ CRUD/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.post_list, name='list')
+    path('', view=views.post_list, name='list'),
+    path('<int:post_id>/', view = views.post_detail, name="detail"),
 ]
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -8,7 +8,19 @@ def post_list(request):
     포스트들을 불러와서 리스트형태로 보여준다.
     '''
 
-    posts = Post.objects.all()
+    posts = Post.objects.all() #READ
     ctx = {'posts': posts}
 
     return render(request, template_name="posts/list.html", context=ctx)
+
+
+def post_detail(request, post_id):
+    '''
+    Read(R)
+    특정 포스트를 불러와서 상세정보를 보여준다.
+    '''
+
+    post = Post.objects.get(id=post_id)
+    ctx = {'post': post}
+
+    return render(request, template_name="posts/detail.html", context=ctx)
